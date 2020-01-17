9bef8488d03093f15fc579344dcb98e0f97054fd
John Ong <john.ong135@gmail.com>
2020-01-17 13:41:13 -0800

    Adding input getters in js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const result = document.getElementById("result");
+
+console.log(hours);
+console.log(minutes);
