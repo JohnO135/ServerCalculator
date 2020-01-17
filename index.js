e88046fdcb314966a2eb39e1a57254ef4ed3afe2
John Ong <john.ong135@gmail.com>
2020-01-17 14:15:44 -0800

    Feature complete

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const result = document.getElementById("result");
+calculate = () => {
+  let hours = parseFloat(document.getElementById("hours").value);
+  let minutes = parseFloat(document.getElementById("minutes").value);
+  const tip = parseFloat(document.getElementById("tip").value);
+  //Converts minutes to decimal value
+  minutes /= 60;
+  //Adds decimal value to hours
+  hours += minutes;
+  //Multiplies hours by hourly rate
+  hours *= 13;
+  //Adds the tip into wage income
+  hours += tip;
+  //Removes approximate 17% tax
+  hours *= 0.83;
 
-console.log(hours);
-console.log(minutes);
+  document.getElementById("result").value = hours;
+};
